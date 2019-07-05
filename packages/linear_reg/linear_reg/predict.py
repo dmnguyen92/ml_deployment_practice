513172a4d6c85a27bbc96e08707135ae10e9d393
dmnguyen92 <dmnguyen92@gmail.com>
2019-07-05 15:16:55 +0800

    setup CircleCI

--- packages/linear_reg/linear_reg/predict.py
+++ packages/linear_reg/linear_reg/predict.py
@@ -15,7 +15,7 @@ pipeline_file_name = f'{config.PIPELINE_SAVE_FILE}{_version}.pkl'
 _model_pipe = load_pipeline(file_name=pipeline_file_name)
 
 def make_prediction(input_data):
-    data = pd.read_json(input_data)
+    data = pd.DataFrame(input_data)
     validated_data = validate_inputs(input_data=data)
     prediction = _model_pipe.predict(validated_data[config.FEATURES])
     
